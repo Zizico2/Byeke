3ed6af321973a9270da0e0ea13d48d2ad6c5cc42
Bernardo António Borda d'Água <berna.agua@gmail.com>
2018-11-17 18:06:41 +0000

    Started commenting Byeke.User.UserInfo

--- src/Byeke/Tags/Nameable.java
+++ src/Byeke/Tags/Nameable.java
@@ -1,5 +1,10 @@
 package Byeke.Tags;
 
 public interface Nameable {
+    //implemented by Park, User
+
+    /**
+     * @return Name
+     */
     String getName();
 }
--- src/Byeke/User/UserInfo.java
+++ src/Byeke/User/UserInfo.java
@@ -5,19 +5,45 @@ import Byeke.Tags.Nameable;
 import dataStructures.Iterator;
 
 public interface UserInfo extends java.io.Serializable, Nameable {
-
+    /**
+     * @return iD: Unique Identifier
+     */
     String getId();
 
+    /**
+     *
+     * @return addess: Address
+     */
     String getAddress();
 
+    /**
+     *
+     * @return emailAddress: Email Address
+     */
     String getEmailAddress();
 
+    /**
+     *
+     * @return phoneNumber: Phone Number
+     */
     String getPhoneNumber();
 
+    /**
+     *
+     * @return tin: Taxpayer Identification Number
+     */
     String getTin();
 
+    /**
+     *
+     * @return balance: Current balance
+     */
     int getBalance();
 
+    /**
+     *
+     * @return points: Current points
+     */
     int getPoints();
 
     boolean hasDelays();
